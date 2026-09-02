f448776c5da1f18e90ea82ba86b6d3598df99398
bird-sitthan <bird.sitthan@gmail.com>
2026-09-02 18:14:45 +0700

    Arai

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const ENV = {
-  PORT: process.env.PORT || 5000,
+  PORT: process.env.PORT || 5001,
   NODE_ENV: process.env.NODE_ENV || 'development',
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/occasion_db',
   JWT_SECRET: process.env.JWT_SECRET || 'occasion_secret_fallback_key_jsd13',
